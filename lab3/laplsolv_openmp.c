ffcc0a8bd9d809eabad86e26cc2bda60024d716d
Linus Mellberg <linme560@student.liu.se>
2013-05-13 13:58:27 +0200

    Faster implentation with less branching.

--- lab3/laplsolv_openmp.c
+++ lab3/laplsolv_openmp.c
@@ -44,19 +44,20 @@ int main(){
   double (*T)[SIZE] = malloc(N*sizeof(double)/sizeof(char));
   double (*prevf)[SIZE] = malloc(n*MAX_THREADS*sizeof(double)/sizeof(char));
   double (*prevl)[SIZE] = malloc(n*MAX_THREADS*sizeof(double)/sizeof(char));
-  int  updatedLines[SIZE], breaklines[MAX_THREADS];
+  int  updatedLines[SIZE];
   double *ptr, *end_ptr;
-  unsigned int error;
+  unsigned int error, glob_error;
   double execTime;
   double tStart, tEnd;
-  int k, i, j, firstLine;
+  int k, i, j;
   int rank, numThreads;
+  int startline, endline;
 
   /* Initialize */
   ptr = (double*)T;
   end_ptr = ptr + N;
   for(; ptr != end_ptr; ++ptr){
-      *ptr = 0.0;
+    *ptr = 0.0;
   }
   for(i = 0; i < n; ++i)
     T[i][0] = 1.0;
@@ -68,71 +69,65 @@ int main(){
   /* Get time */
   tStart = omp_get_wtime();
 
-#pragma omp parallel shared(error, tol, T, k) private(i,j, rank, numThreads) default(shared)
+  glob_error = 1;
+#pragma omp parallel shared(tol, T, k, glob_error) private(i,j, rank, numThreads, error, startline, endline)
   {
     /* Run calculation */
     rank = omp_get_thread_num();
     numThreads = omp_get_num_threads();
     if(rank == 0) printf("Running with %d threads.\n", numThreads);
-    error = 1;
-    for(k = 0; k < maxiter && error;){
+    startline = (rank*n1)/numThreads;
+    endline = ((rank+1)*n1)/numThreads;
+    if(rank == 0) ++startline;
+    if(endline == n1) ++endline;
+    for(k = 0; k < maxiter && glob_error;){
+      int first = 1;
 #pragma omp single
+      {
         ++k;
+      }
       error = 0;
-      firstLine = 1;
-#pragma omp for schedule(static) private(j)
-      for(j = 0; j < n; ++j){
-        /*printf("rank: %d j: %d\n", rank, j);*/
-        /*prev[j] = T[0][j];*/
-        updatedLines[j] = 0;
-      }
-#pragma omp for firstprivate(firstLine) private(i,j) schedule(static) reduction(|| : error)
-      for(i = 1; i < n1; ++i){
-        int uu;
-        breaklines[rank] = i;
-        if(firstLine){
-          firstLine = 0;
-          updatedLines[i] = rank+1;
-          memcpy(prevl[rank], T[i-1], n*sizeof(double)/sizeof(char));
-          memcpy(prevf[rank], T[i], n*sizeof(double)/sizeof(char));
-        }
-        uu = updatedLines[i+1];
+      glob_error = 0;
 
-        /*Ordering of if and for is important to prevent branching inside the loop.
-         * Two for loops are better than one.*/
-        if(uu){
-          --uu;
-          for(j = 1; j < n1; ++j){
-            double tmp = prevl[rank][j];
-            prevl[rank][j] = (T[i-1][j] + prevf[uu][j] + T[i][j+1] + T[i][j-1])/4.0;
-            T[i-1][j] = tmp;
+      memcpy(prevl[rank], T[startline-1], n*sizeof(double)/sizeof(char));
+      memcpy(prevf[rank], T[startline], n*sizeof(double)/sizeof(char));
+      //#pragma omp for firstprivate(firstLine) private(i,j) schedule(static) reduction(|| : error)
+#pragma omp barrier
+      for(i = startline; i < endline-1; ++i){
+        for(j = 1; j < n1; ++j){
+          double tmp = prevl[rank][j];
+          prevl[rank][j] = (T[i-1][j] + T[i+1][j] + T[i][j+1] + T[i][j-1])/4.0;
+          T[i-1][j] = tmp;
 
-            /*Tried some different approaches for error calculation, this seems to be the fastest.*/
-            error = error || fabs(T[i][j] - prevl[rank][j]) > tol;
-            /*error = max(error, fabs(T[i][j] - prevl[rank][j]));*/
-            /*diff = T[i][j] - prevl[rank][j];
+          /*Tried some different approaches for error calculation, this seems to be the fastest.*/
+          error = error || fabs(T[i][j] - prevl[rank][j]) > tol;
+          /*if(error && first) printf("Error1 in thread: %d %d %d %f\n", rank, i, j, fabs(T[i][j] - prevl[rank][j])), first = 0;*/
+          /*if(error < fabs(T[i][j] - prevl[rank][j])) printf("%d %d %f %f\n", i, j, T[i][j], prevl[rank][j]);*/
+          /*error = max(error, fabs(T[i][j] - prevl[rank][j]));*/
+          /*diff = T[i][j] - prevl[rank][j];
             error = error || diff > tol || -diff > tol;*/
-          }
-        }
-        else{
-          for(j = 1; j < n1; ++j){
-            double tmp = prevl[rank][j];
-            prevl[rank][j] = (T[i-1][j] + T[i+1][j] + T[i][j+1] + T[i][j-1])/4.0;
-            T[i-1][j] = tmp;
-
-            error = error || fabs(T[i][j] - prevl[rank][j]) > tol;
-            /*if(error < fabs(T[i][j] - prevl[rank][j])) printf("%d %d %f %f\n", i, j, T[i][j], prevl[rank][j]);*/
-            /*error = max(error, fabs(T[i][j] - prevl[rank][j]));*/
-            /*diff = T[i][j] - prevl[rank][j];
-            error = error || diff > tol || -diff > tol;*/
-          }
         }
       }
-      /*printf("error %f\n", error);*/
+      if(rank+1<numThreads){
+        for(j = 1; j < n1; ++j){
+          double tmp = prevl[rank][j];
+          prevl[rank][j] = (T[i-1][j] + prevf[rank+1][j] + T[i][j+1] + T[i][j-1])/4.0;
+          T[i-1][j] = tmp;
+
+          error = error || fabs(T[i][j] - prevl[rank][j]) > tol;
+        }
+        ++i;
+      }
+#pragma omp barrier
       for(j = 1; j < n1; ++j){
-        T[breaklines[rank]][j] = prevl[rank][j];
+        T[i-1][j] = prevl[rank][j];
       }
-/*#pragma omp flush*/
+#pragma omp critical
+      {
+        glob_error = glob_error || error;
+      }
+#pragma omp barrier
+#pragma omp flush(glob_error,k)
     }
     /*fprintf(stderr, "error: %d tol: %f k: %d maxiter: %d rank: %d\n", error, tol, k, maxiter, rank);*/
   }
