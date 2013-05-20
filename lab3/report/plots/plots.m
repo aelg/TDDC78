e8856008b816be381ed016f0cb448b35eeb075b3
Oskar Aagaard Fransson <oskaa489@student.liu.se>
2013-05-20 16:05:33 +0200

    lab 3 rapport resultat klart

--- lab3/report/plots/plots.m
+++ lab3/report/plots/plots.m
@@ -1,5 +1,5 @@
 threads = [1 2 4 8 16];
-size = [1000*1000 1414*1414 2000*2000 2828*2828 4000*4000 10000*10000];
+size = [1000 1414 2000 2828 4000 10000];
 
 n_1000_time = [0.860502 0.464044 0.308580 0.174112 0.216558];
 n_1000_flops_t = [3242.293583 3000.792351 2256.303586 1999.430418 803.768563];
@@ -31,7 +31,7 @@ flops_t = [n_1000_flops_t; n_1414_flops_t; n_2000_flops_t; n_2828_flops_t; n_400
 figure(1);
 plot(threads, n_1000_time, 'b-x', threads, n_1414_time, 'r-x', threads, n_2000_time, 'g-x', threads, n_2828_time, 'y-x', threads, n_4000_time, 'c-x');
 xlabel('Threads');
-ylabel('Execution time [t]')
+ylabel('Execution time [s]')
 legend('N = 1000', 'N = 1414', 'N = 2000', 'N = 2828', 'N = 4000');
 
 figure(2);
@@ -42,13 +42,19 @@ legend('N = 1000', 'N = 1414', 'N = 2000', 'N = 2828', 'N = 4000', 'Location', '
 
 figure(3);
 plot(size, flops(1,:), 'b-x', size, flops(2,:), 'r-x', size, flops(3,:), 'g-x', size, flops(4,:), 'y-x', size, flops(5,:), 'c-x');
-xlabel('Problem Size (N*N)');
+xlabel('Problem Size (N)');
 ylabel('Total MFLOPS')
 legend('1', '2', '4', '8', '16', 'Location', 'Northeast');
 
 figure(4);
 plot(size, flops_t(1,:), 'b-x', size, flops_t(2,:), 'r-x', size, flops_t(3,:), 'g-x', size, flops_t(4,:), 'y-x', size, flops_t(5,:), 'c-x');
-xlabel('Problem Size (N*N)');
+xlabel('Problem Size (N)');
 ylabel('MFLOPS per thread');
 ylim([0 3500]);
 legend('1', '2', '4', '8', '16', 'Location', 'Northeast');
+
+figure(5);
+plot(threads,[n_1000_time(1) n_1414_time(2) n_2000_time(3) n_2828_time(4) n_4000_time(5)], 'b-x');
+xlabel('Problem size / Threads');
+ylabel('Execution time [s]');
+
